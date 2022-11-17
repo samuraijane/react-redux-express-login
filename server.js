aaa82af59e529908bccf8658e2b3a98041899c2a
Matthew Day <matt@matthewday.net>
2022-11-16 21:16:03 -0500

    add POST for /login

--- server.js
+++ server.js
@@ -1,9 +1,27 @@
 const express = require('express');
+const { User } = require('./models');
 
 const server = express();
 
+server.use(express.json());
+
 server.get('/heartbeat', (req, res) => {
   res.json({is: 'working'});
+});
+
+server.post('/login', async (req, res) => {
+  const { password, username } = req.body;
+  const user = await User.findOne({
+    where: {
+      password,
+      username
+    }
+  });
+  if (user) {
+    res.json({isSuccess: true});
+  } else {
+    res.json({isSuccess: false});
+  }
 });
 
 server.listen(8080, () => {
