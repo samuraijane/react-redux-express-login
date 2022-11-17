789816237b12294fc5f7758f1d4be23641a8a3e2
Matthew Day <matt@matthewday.net>
2022-11-16 23:42:06 -0500

    add navs to header

--- react-ui/src/sectioning/header.jsx
+++ react-ui/src/sectioning/header.jsx
@@ -1,9 +1,19 @@
-const Header = () => (
-  <header>
-    <div className="y-wrap">
-      I am header
-    </div>
-  </header>
-);
+import { NavLink } from 'react-router-dom';
+import navs from '../configs/navs.json';
+
+const Header = () => {
+
+  const _navs = navs.map((nav, index) => {
+    return <NavLink key={`${index}-${nav.href}`} to={nav.href}>{nav.text}</NavLink>
+  });
+
+  return (
+    <header>
+      <div className="y-wrap">
+        <nav>{_navs}</nav>
+      </div>
+    </header>
+  );
+};
 
 export default Header;
