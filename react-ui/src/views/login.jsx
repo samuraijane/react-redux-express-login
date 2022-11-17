2b2965269d69bf38481af9d9354209c626780c16
Matthew Day <matt@matthewday.net>
2022-11-17 00:26:15 -0500

    add login button

--- react-ui/src/views/login.jsx
+++ react-ui/src/views/login.jsx
@@ -1,7 +1,19 @@
-const Login = () => (
-  <div>
-    I am login
-  </div>
-);
+import { useDispatch } from 'react-redux';
+import { doLogin } from '../features/authenticationSlice';
+
+const Login = () => {
+
+  const dispatch = useDispatch();
+
+  const handleClick = e => {
+    dispatch(doLogin());
+  };
+
+  return (
+    <div>
+      <button onClick={handleClick}>Click me to login</button>
+    </div>
+  );
+};
 
 export default Login;
