5117bdc77b817d74c0cec01f9c81e55137c802cc
Matthew Day <matt@matthewday.net>
2022-11-27 21:05:53 -0500

    define prod env variables

--- config/config.js
+++ config/config.js
@@ -1,3 +1,11 @@
+const {
+  PG_DATABASE,
+  PG_HOST,
+  PG_PASSWORD,
+  PG_PORT,
+  PG_USER
+} = process.env;
+
 module.exports = {
   development: {
     username: "admin",
@@ -14,10 +22,10 @@ module.exports = {
     dialect: "postgres"
   },
   production: {
-    username: "root",
-    password: null,
-    database: "react-redux-express-login_prod",
-    host: "127.0.0.1",
+    username: PG_USER,
+    password: PG_PASSWORD,
+    database: PG_DATABASE,
+    host: `${PG_HOST}:${PG_PORT}`,
     dialect: "postgres"
   }
 };
